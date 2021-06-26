8874c91d26af150dea7f21e998f43e9e64315f2b
Paritosh <tiwariparitosh9999@gmail.com>
2021-06-26 17:30:19 +0530

    * UserModel.cs: * LoginController.cs:
    * appsettings.json: JWT Implemented

---
+++ APIs/RandDAPI/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿// UserModel.cs/Users/paritoshmacbook/Projects/RandD/RandD/APIs/RandDAPI/Models/Users/paritoshmacbook/Projects/RandD/RandD/APIs/RandDAPI/Models/UserModel.csParitosh Tiwaritiwari.paritosh@outlook.comParitosh Tiwari 2021*/UserModel
+using System;
+namespace RandDAPI.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
